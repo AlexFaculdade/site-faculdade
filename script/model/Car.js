931e9948043816e08df5d0fb46f1808864721b7c
ALEXSANDRO KRUMHEUER FILHO <alexsandro.filho@cs.up.edu.br>
2025-10-24 11:24:17 -0300

    Começando página de comprar

---
+++ script/model/Car.js
@@ -0,0 +1,20 @@
+class Car {
+    marca
+    modelo
+    cor
+    chassi
+    anoFabricacao
+    anoModelo
+    quilometragem
+    motor
+    constructor(marcam, modelo, cor, chassi, anoFabricacao, anoModelo, quilometragem, motor) {
+        this.marca = marca
+        this.modelo = modelo
+        this.cor = cor
+        this.chassi = chassi
+        this.anoFabricacao = anoFabricacao
+        this.anoModelo = anoModelo
+        this.quilometragem = quilometragem
+        this.motor = motor
+    }
+}
